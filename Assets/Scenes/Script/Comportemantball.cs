a74cf5f7adc6fc50b0fabfe82fa48b68febfc1a4
Nira00066 <lnina@00066gmail.com>
2024-07-25 09:53:25 +0200

    debut scritp

--- Assets/Scenes/Script/Comportemantball.cs
+++ Assets/Scenes/Script/Comportemantball.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Comportemantball : MonoBehaviour
 {
-    // Start is called before the first frame update
+       Gyroscope m_Gyro;
+
     void Start()
     {
-        
+        //Set up and enable the gyroscope (check your device has one)
+        m_Gyro = Input.gyro;
+        m_Gyro.enabled = true;
     }
 
-    // Update is called once per frame
-    void Update()
+//This is a legacy function, check out the UI section for other ways to create your UI
+    void OnGUI()
     {
-        
+        //Output the rotation rate, attitude and the enabled state of the gyroscope as a Label
+        GUI.Label(new Rect(500, 300, 200, 0), "Gyro rotation rate " + m_Gyro.rotationRate);
+        GUI.Label(new Rect(500, 350, 200, 0), "Gyro attitude" + m_Gyro.attitude);
+        GUI.Label(new Rect(500, 400, 200, 0), "Gyro enabled : " + m_Gyro.enabled);
     }
 }
