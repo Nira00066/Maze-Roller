a77a428e1e68c6840ee4dbb2f1fe73d581e9b9e8
Nira00066 <lnina@00066gmail.com>
2024-07-25 13:30:31 +0200

    a check?

--- Assets/Scenes/Script/Comportemantball.cs
+++ Assets/Scenes/Script/Comportemantball.cs
@@ -6,40 +6,49 @@ public class Comportemantball : MonoBehaviour
 {
     [SerializeField] GameObject ball;
     private Rigidbody RgbBall;
-       Gyroscope m_Gyro;
+    private Gyroscope m_Gyro;
 
     void Start()
     {
         RgbBall = GetComponent<Rigidbody>();
 
-        //Set up and enable the gyroscope (check your device has one)
-        m_Gyro = Input.gyro;
-        m_Gyro.enabled = true;
+        // Set up and enable the gyroscope (check your device has one)
+        if (SystemInfo.supportsGyroscope)
+        {
+            m_Gyro = Input.gyro;
+            m_Gyro.enabled = true;
+        }
+        else
+        {
+            Debug.LogError("Gyroscope non supporté sur cet appareil.");
+        }
     }
 
-//This is a legacy function, check out the UI section for other ways to create your UI
     void OnGUI()
     {
-        //Output the rotation rate, attitude and the enabled state of the gyroscope as a Label
-        GUI.Label(new Rect(500, 300, 200, 0), "Gyro rotation rate " + m_Gyro.rotationRate);
-        GUI.Label(new Rect(500, 350, 200, 0), "Gyro attitude" + m_Gyro.attitude);
-        GUI.Label(new Rect(500, 400, 200, 0), "Gyro enabled : " + m_Gyro.enabled);
+        // Output the rotation rate, attitude and the enabled state of the gyroscope as a Label
+        GUI.Label(new Rect(500, 300, 300, 20), "Gyro rotation rate: " + m_Gyro.rotationRate);
+        GUI.Label(new Rect(500, 350, 300, 20), "Gyro attitude: " + m_Gyro.attitude);
+        GUI.Label(new Rect(500, 400, 300, 20), "Gyro enabled: " + m_Gyro.enabled);
     }
 
     void Update()
     {
-// debugger pour voir si on accède au gyroscope 
-
+        // Debug to check if the gyroscope is accessed
         if (Input.gyro.enabled)
         {
             // Lire les données du gyroscope
             Vector3 gyroRotation = Input.gyro.rotationRateUnbiased;
 
             // Appliquer une force à la balle en fonction du gyroscope
-            RgbBall.AddForce(new Vector3(gyroRotation.x, 0, gyroRotation.y) * Time.deltaTime * 500);
+            Vector3 force = new Vector3(gyroRotation.x, 0, gyroRotation.y) * 500;
+            RgbBall.AddForce(force * Time.deltaTime);
+
+            // Debug to see the force being applied
+            Debug.Log("Gyro rotation rate: " + gyroRotation);
+            Debug.Log("Force applied: " + force);
         }
-
-
     }
 }
+
 //voir si c'est bon 
